abac676990af980e593f8dbc5c0c6b8379d8880b
Vishal Seenivasan <vseenivasan@ucsb.edu>
2025-03-14 17:45:07 -0700

    Server code from Gemini

--- HW 3/hangman_server.c
+++ HW 3/hangman_server.c
@@ -3,167 +3,195 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
-#include <time.h>
 #include <ctype.h>
+#include <time.h>
+#include <sys/select.h>
 
-#define PORT 8080
 #define MAX_CLIENTS 3
-#define BUFFER_SIZE 128
+#define MAX_WORD_LENGTH 8
+#define MAX_INCORRECT_GUESSES 6
+#define PORT 8081
 #define MAX_WORDS 15
-#define MAX_INCORRECT 6
+#define MAX_BUFFER_SIZE 256
 
 typedef struct {
     int socket;
-    char word[9];
-    char display[9];
-    char incorrect[7];
+    char word[MAX_WORD_LENGTH + 1];
+    char display[MAX_WORD_LENGTH + 1];
+    char incorrect_guesses[MAX_INCORRECT_GUESSES + 1];
     int incorrect_count;
-} ClientData;
+    int in_use;
+    int game_over;
+} ClientState;
 
-char words[MAX_WORDS][9];
-int word_count = 0;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
-void load_words() {
+void load_words(char words[][MAX_WORD_LENGTH + 1], int *count) {
     FILE *file = fopen("hangman_words.txt", "r");
     if (!file) {
-        perror("Error opening word file");
+        perror("Error opening hangman_words.txt");
         exit(EXIT_FAILURE);
     }
-    
-    while (fgets(words[word_count], sizeof(words[word_count]), file)) {
-        words[word_count][strcspn(words[word_count], "\n")] = '\0';
-        word_count++;
-        if (word_count >= MAX_WORDS) break;
+    *count = 0;
+    while (fscanf(file, "%s", words[*count]) != EOF && *count < MAX_WORDS) {
+        int len = strlen(words[*count]);
+        if (len >= 3 && len <= 8) {
+            (*count)++;
+        }
     }
-    
     fclose(file);
-}
-
-char* choose_random_word() {
-    return words[rand() % word_count];
-}
-
-void send_game_state(ClientData *client, int msg_flag, const char *message) {
-    char buffer[BUFFER_SIZE];
-    memset(buffer, 0, BUFFER_SIZE);
-    
-    if (msg_flag > 0) {
-        buffer[0] = msg_flag;
-        strcpy(buffer + 1, message);
-        send(client->socket, buffer, msg_flag + 1, 0);
-    } else {
-        buffer[0] = 0;
-        buffer[1] = strlen(client->word);
-        buffer[2] = client->incorrect_count;
-        strcpy(buffer + 3, client->display);
-        strcat(buffer + 3 + strlen(client->word), client->incorrect);
-        send(client->socket, buffer, BUFFER_SIZE, 0);
+    if (*count == 0) {
+        fprintf(stderr, "Error: No valid words found in hangman_words.txt\n");
+        exit(EXIT_FAILURE);
     }
 }
 
-void *handle_client(void *arg) {
-    ClientData client = *(ClientData *)arg;
-    char buffer[BUFFER_SIZE];
-    
-    recv(client.socket, buffer, BUFFER_SIZE, 0);
-    send_game_state(&client, 0, NULL);
-    
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        int bytes_read = recv(client.socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_read <= 0) break;
+char *get_random_word(char words[][MAX_WORD_LENGTH + 1], int count) {
+    return words[rand() % count];
+}
 
-        char guess = tolower(buffer[0]);
+void initialize_client(ClientState *client, int client_socket, char *word) {
+    client->socket = client_socket;
+    strcpy(client->word, word);
+    memset(client->display, '_', strlen(word));
+    client->display[strlen(word)] = '\0';
+    memset(client->incorrect_guesses, 0, sizeof(client->incorrect_guesses));
+    client->incorrect_count = 0;
+    client->in_use = 1;
+    client->game_over = 0;
+}
+
+void send_game_state(int client_socket, char *display, char *incorrect_guesses, int word_length, int incorrect_count) {
+    char buffer[MAX_BUFFER_SIZE];
+    snprintf(buffer, sizeof(buffer), "%c%c%c%s%s", 0, (char)word_length, (char)incorrect_count, display, incorrect_guesses);
+    send(client_socket, buffer, word_length + incorrect_count + 3, 0);
+}
+
+void send_message(int client_socket, char *message) {
+    char buffer[MAX_BUFFER_SIZE];
+    int message_length = strlen(message);
+    buffer[0] = (char)message_length;
+    memcpy(buffer + 1, message, message_length);
+    send(client_socket, buffer, message_length + 1, 0);
+}
+
+void handle_client(ClientState *client) {
+    if (client->game_over) {
+        return;
+    }
+
+    char guess;
+    int bytes_received = recv(client->socket, &guess, 1, MSG_DONTWAIT);
+
+    if (bytes_received > 0) {
+        guess = tolower(guess);
+        printf("Received guess %c\n", guess);
+
         int correct = 0;
-
-        for (int i = 0; i < strlen(client.word); i++) {
-            if (client.word[i] == guess) {
-                client.display[i] = guess;
+        for (int i = 0; i < strlen(client->word); i++) {
+            if (client->word[i] == guess) {
+                client->display[i] = guess;
                 correct = 1;
             }
         }
 
-        if (!correct) {
-            client.incorrect[client.incorrect_count++] = guess;
+        if (!correct && strchr(client->incorrect_guesses, guess) == NULL) {
+            client->incorrect_guesses[client->incorrect_count] = guess;
+            client->incorrect_count++;
         }
 
-        if (client.incorrect_count >= MAX_INCORRECT) {
-            send_game_state(&client, 8, "You Lose :(");
-            break;
+        if (strcmp(client->word, client->display) == 0) {
+            send_message(client->socket, "You Win!");
+            send_message(client->socket, "Game Over!");
+            client->game_over = 1;
+        } else if (client->incorrect_count >= MAX_INCORRECT_GUESSES) {
+            send_message(client->socket, "You Lose");
+            send_message(client->socket, "Game Over!");
+            client->game_over = 1;
         }
 
-        if (strcmp(client.word, client.display) == 0) {
-            send_game_state(&client, 8, "You Win!");
-            break;
-        }
+        send_game_state(client->socket, client->display, client->incorrect_guesses, strlen(client->word), client->incorrect_count);
 
-        send_game_state(&client, 0, NULL);
+        if (client->game_over) {
+            close(client->socket);
+            client->in_use = 0;
+        }
+    } else if (bytes_received == 0) {
+        client->in_use = 0;
+        client->game_over = 1;
+        close(client->socket);
     }
-    
-    send_game_state(&client, 8, "Game Over!");
-    close(client.socket);
-    free(arg);
-    return NULL;
 }
 
 int main() {
-    srand(time(NULL));
-    load_words();
-    
     int server_fd, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size;
-    
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    char words[MAX_WORDS][MAX_WORD_LENGTH + 1];
+    int word_count;
+    ClientState clients[MAX_CLIENTS] = {0};
+
+    srand(time(NULL));
+    load_words(words, &word_count);
+
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, MAX_CLIENTS);
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, MAX_CLIENTS) == -1) {
-        perror("Listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Hangman Server running on port %d...\n", PORT);
+    fd_set readfds;
+    int max_sd, sd;
 
     while (1) {
-        addr_size = sizeof(client_addr);
-        client_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size);
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
 
-        if (client_socket == -1) {
-            perror("Client connection failed");
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = clients[i].socket;
+            if (clients[i].in_use) {
+                FD_SET(sd, &readfds);
+                if (sd > max_sd) {
+                    max_sd = sd;
+                }
+            }
+        }
+
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if (activity < 0) {
+            perror("select error");
             continue;
         }
 
-        pthread_mutex_lock(&lock);
-        ClientData *client_data = malloc(sizeof(ClientData));
-        client_data->socket = client_socket;
-        strcpy(client_data->word, choose_random_word());
-        memset(client_data->display, '_', strlen(client_data->word));
-        client_data->display[strlen(client_data->word)] = '\0';
-        memset(client_data->incorrect, 0, 7);
-        client_data->incorrect_count = 0;
-        pthread_mutex_unlock(&lock);
+        if (FD_ISSET(server_fd, &readfds)) {
+            client_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+            if (client_socket >= 0) {
+                int client_index = -1;
+                for (int i = 0; i < MAX_CLIENTS; i++) {
+                    if (!clients[i].in_use) {
+                        client_index = i;
+                        break;
+                    }
+                }
 
-        pthread_t thread;
-        pthread_create(&thread, NULL, handle_client, client_data);
-        pthread_detach(thread);
+                if (client_index == -1) {
+                    send_message(client_socket, "Server Overloaded");
+                    close(client_socket);
+                } else {
+                    char *word = get_random_word(words, word_count);
+                    initialize_client(&clients[client_index], client_socket, word);
+                    send_game_state(clients[client_index].socket, clients[client_index].display, clients[client_index].incorrect_guesses, strlen(clients[client_index].word), clients[client_index].incorrect_count);
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            sd = clients[i].socket;
+            if (clients[i].in_use && FD_ISSET(sd, &readfds)) {
+                handle_client(&clients[i]);
+            }
+        }
     }
-
-    close(server_fd);
     return 0;
 }
